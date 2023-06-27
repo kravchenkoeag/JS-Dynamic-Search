145b89a3537225aab0529e9dd4ca758f0bb0744c
kravchenkoeag <kravchenkoeag@gmail.com>
2023-06-28 00:10:16 +0300

    modified:   code.js

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 const api = 
-`https://gist.githubusercontent.com/VasilyMur
-/43ef6df83bba694f871f11a16ed7556d/raw/
-b6edff674e35452d6c57ec64177a558f7adb432e/moscowSubway.json`;
+`https://raw.githubusercontent.com/kravchenkoeag/JS-Dynamic-Search/main/subway.json`;
 
 const stations = [];
 
